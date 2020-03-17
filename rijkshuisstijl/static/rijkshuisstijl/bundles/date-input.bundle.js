abf948fda5b5a8d80f5de1ba83379e67ed1a3e9a
Sonny Bakker <sonny871@hotmail.com>
2020-03-17 10:27:29 +0100

    Hide buttons inside the summary block

--- rijkshuisstijl/static/rijkshuisstijl/bundles/date-input.bundle.js
+++ rijkshuisstijl/static/rijkshuisstijl/bundles/date-input.bundle.js
@@ -1,2 +1,44 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{316:function(e,t,n){"use strict";n.r(t);var a=n(5),r=n.n(a),o=n(305),i=n.n(o),u=n(306),c=n(45);function l(e){return function(e){if(Array.isArray(e)){for(var t=0,n=new Array(e.length);t<e.length;t++)n[t]=e[t];return n}}(e)||function(e){if(Symbol.iterator in Object(e)||"[object Arguments]"===Object.prototype.toString.call(e))return Array.from(e)}(e)||function(){throw new TypeError("Invalid attempt to spread non-iterable instance")}()}function d(e,t){for(var n=0;n<t.length;n++){var a=t[n];a.enumerable=a.enumerable||!1,a.configurable=!0,"value"in a&&(a.writable=!0),Object.defineProperty(e,a.key,a)}}var s=function(){function e(t){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,e),this.node=t,this.update()}var t,n,a;return t=e,(n=[{key:"getDateFormat",value:function(){return this.node.dataset.dateFormat?this.node.dataset.dateFormat:this.isDateTime()?"d-m-Y H:1":"d-m-Y"}},{key:"getLocale",value:function(){var e=u.Dutch;return e.firstDayOfWeek=0,e}},{key:"getMode",value:function(){return r.a.hasModifier(this.node,c.h)?"range":"single"}},{key:"isDateTime",value:function(){return"datetime"===this.node.type}},{key:"updatePlaceholder",value:function(){if(!this.node.placeholder){var e=this.getDateFormat().replace("d","dd").replace("m","mm").replace("Y","yyyy");this.node.placeholder=e}}},{key:"update",value:function(){this.updatePlaceholder(),i()(this.node,{altInput:!0,altFormat:this.getDateFormat(),dateFormat:"Y-m-d",defaultDate:this.node.value.split("/"),locale:this.getLocale(),mode:this.getMode()}).l10n.rangeSeparator="/"}}])&&d(t.prototype,n),a&&d(t,a),e}();[].concat(l(c.c),l(c.d)).forEach((function(e){return new s(e)}))}}]);
-//# sourceMappingURL=date-input.bundle.js.map
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["date-input"],{
+
+/***/ "./rijkshuisstijl/js/components/form/date-input.js":
+/*!*********************************************************!*\
+  !*** ./rijkshuisstijl/js/components/form/date-input.js ***!
+  \*********************************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var bem_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bem.js */ "./node_modules/bem.js/dist/bem.js");
+/* harmony import */ var bem_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(bem_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var flatpickr__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! flatpickr */ "./node_modules/flatpickr/dist/flatpickr.js");
+/* harmony import */ var flatpickr__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(flatpickr__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var flatpickr_dist_l10n_nl__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! flatpickr/dist/l10n/nl */ "./node_modules/flatpickr/dist/l10n/nl.js");
+/* harmony import */ var flatpickr_dist_l10n_nl__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(flatpickr_dist_l10n_nl__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _constants__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./constants */ "./rijkshuisstijl/js/components/form/constants.js");
+function _toConsumableArray(arr){return _arrayWithoutHoles(arr)||_iterableToArray(arr)||_nonIterableSpread();}function _nonIterableSpread(){throw new TypeError("Invalid attempt to spread non-iterable instance");}function _iterableToArray(iter){if(Symbol.iterator in Object(iter)||Object.prototype.toString.call(iter)==="[object Arguments]")return Array.from(iter);}function _arrayWithoutHoles(arr){if(Array.isArray(arr)){for(var i=0,arr2=new Array(arr.length);i<arr.length;i++){arr2[i]=arr[i];}return arr2;}}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}function _createClass(Constructor,protoProps,staticProps){if(protoProps)_defineProperties(Constructor.prototype,protoProps);if(staticProps)_defineProperties(Constructor,staticProps);return Constructor;}/**
+ * Adds a datepicker to date inputs.
+ * @class
+ */var DateInput=/*#__PURE__*/function(){/**
+     * Constructor method.
+     * @param {HTMLInputElement} node
+     */function DateInput(node){_classCallCheck(this,DateInput);/** @type {HTMLInputElement} */this.node=node;this.update();}/**
+     * Returns the date format to use with the datepicker.
+     * @return {string}
+     */_createClass(DateInput,[{key:"getDateFormat",value:function getDateFormat(){if(this.node.dataset.dateFormat){return this.node.dataset.dateFormat;}return this.isDateTime()?'d-m-Y H:1':'d-m-Y';}/**
+     * Returns the (Dutch) locale to use.
+     * @return {CustomLocale}
+     */},{key:"getLocale",value:function getLocale(){var locale=flatpickr_dist_l10n_nl__WEBPACK_IMPORTED_MODULE_2__["Dutch"];locale.firstDayOfWeek=0;return locale;}/**
+     * Returns the mode to use, either "range" or "single".
+     * @return {string}
+     */},{key:"getMode",value:function getMode(){return bem_js__WEBPACK_IMPORTED_MODULE_0___default.a.hasModifier(this.node,_constants__WEBPACK_IMPORTED_MODULE_3__["MODIFIER_DATE_RANGE"])?'range':'single';}/**
+     * @TODO: Yet to be supported.
+     * @return {boolean}
+     */},{key:"isDateTime",value:function isDateTime(){return this.node.type==='datetime';}},{key:"updatePlaceholder",value:function updatePlaceholder(){if(!this.node.placeholder){var placeholder=this.getDateFormat().replace('d','dd').replace('m','mm').replace('Y','yyyy');this.node.placeholder=placeholder;}}/**
+     * Adds the datepicker.
+     */},{key:"update",value:function update(){this.updatePlaceholder();var flatPicker=flatpickr__WEBPACK_IMPORTED_MODULE_1___default()(this.node,{altInput:true,altFormat:this.getDateFormat(),dateFormat:'Y-m-d',defaultDate:this.node.value.split('/'),locale:this.getLocale(),mode:this.getMode()});flatPicker.l10n.rangeSeparator='/';}}]);return DateInput;}();// Start!
+[].concat(_toConsumableArray(_constants__WEBPACK_IMPORTED_MODULE_3__["DATE_INPUTS"]),_toConsumableArray(_constants__WEBPACK_IMPORTED_MODULE_3__["DATE_RANGE_INPUTS"])).forEach(function(node){return new DateInput(node);});
+
+/***/ })
+
+}]);
